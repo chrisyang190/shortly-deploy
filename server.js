3d0f74d9f7d8264429d577c2777bd860e543cdbe
Hack Reactor Students <accounts@hackreactor.com>
2017-03-08 16:37:01 -0800

    further tests

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var hello = 3;
+
 var app = require('./server-config.js');
 
 var port = 4568;
 
 app.listen(port);
-
+console.log('we are working!');
 console.log('Server now listening on port ' + port);
